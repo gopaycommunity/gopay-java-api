f00666a9bcea4638696713d62db62810e583d733
Frantisek Sichinger <frantisek.sichinger@gopay.cz>
2016-01-29 11:42:15 +0100

    Uprava pom pro maven central

--- common/src/main/java/cz/gopay/api/v3/model/access/OAuth.java
+++ common/src/main/java/cz/gopay/api/v3/model/access/OAuth.java
@@ -10,4 +10,5 @@ public class OAuth {
     public static String GRANT_TYPE_CLIENT_CREDENTIALS = "client_credentials";
     public static String SCOPE_PAYMENT_ALL = "payment-all";
     public static String SCOPE_PAYMENT_CREATE = "payment-create";
+    public static String TOKEN_TYPE_BEARER = "bearer";
 }
